fcc8a0f881f9e96c350428672181d16999f69ad8
ohooviii <viviraan@gmail.com>
2022-02-04 17:33:16 -0500

    02.04.22_1

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 
 $(document).ready(function(){
 
+    $(".cat_click").click(function(){
+        alert("Thank you for this wintersession!");
+    });
+
+    $(".cat_click1").click(function(){
+        alert("Zach and Serena You are the best!");
+    });
+
     $(".cat_drag").draggable();
     
 })
